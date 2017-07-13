9c2634e26b7872a6ff61cfb7739b301ebe75831c
min16 <mjna231@gmail.com>
2017-07-13 17:35:13 +0900

    Add Problem 24

---
+++ src/linkedlist/SwapNodesInPairs.java
@@ -0,0 +1,23 @@
+package linkedlist;
+
+public class SwapNodesInPairs {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode current = dummyHead;
+        while (current.next != null && current.next.next != null) {
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+            first.next = second.next;
+            current.next = second;
+            current.next.next = first;
+            current = current.next.next;
+        }
+        return dummyHead.next;
+    }
+}
