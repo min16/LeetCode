508033b0de8a8c6a054f023642456af78bc5d383
min16 <mjna231@gmail.com>
2017-09-30 11:24:32 +0900

    Create method productExceptSelf

--- src/array_string/ProductOfArrayExceptSelf.java
+++ src/array_string/ProductOfArrayExceptSelf.java
@@ -3,8 +3,21 @@ package array_string;
 /**
  * Problem 238
  */
-//public class ProductOfArrayExceptSelf {
-//    public int[] productExceptSelf(int[] nums) {
-//
-//    }
-//}
+
+public class ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int size = nums.length;
+        int[] productExceptSelf = new int[size];
+        for (int i = 0, beforeIndex = 1; i < size; i ++) {
+            productExceptSelf[i] = beforeIndex;
+            beforeIndex *= nums[i];
+        }
+
+        for (int i = size - 1, afterIndex = 1; i >= 0; i --) {
+            productExceptSelf[i] *= afterIndex;
+            afterIndex *= nums[i];
+        }
+
+        return productExceptSelf;
+    }
+}
