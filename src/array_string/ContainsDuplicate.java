f1e34ef71126ce37dc2529d4e21a178753caa61e
min16 <mjna231@gmail.com>
2017-10-05 09:57:51 +0900

    Update problem 217

--- src/array_string/ContainsDuplicate.java
+++ src/array_string/ContainsDuplicate.java
@@ -1,7 +1,20 @@
 package array_string;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * Created by USER-PC on 2017-10-03.
+ * Problem 217
  */
 public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        int size = nums.length;
+        if (size < 2) return false;
+        Set<Integer> set = new HashSet<Integer>();
+        for (int number : nums) {
+            if (set.contains(number)) return true;
+            set.add(number);
+        }
+        return false;
+    }
 }
