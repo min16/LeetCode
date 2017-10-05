cdba130a7acb970337f22daae3cc31e4719dca44
min16 <mjna231@gmail.com>
2017-10-05 09:58:21 +0900

    Update problem 198

--- src/dp/HouseRobber.java
+++ src/dp/HouseRobber.java
@@ -1,7 +1,21 @@
 package dp;
 
 /**
- * Created by USER-PC on 2017-10-05.
+ * Problem 198
  */
 public class HouseRobber {
+    public int rob(int[] nums) {
+        int ifRobbedPrev = 0;
+        int ifNotRobbedPrev = 0;
+
+        for (int i = 0; i < nums.length; i ++) {
+            int currRobbed = ifNotRobbedPrev + nums[i];
+            int currNotRobbed = Math.max(ifNotRobbedPrev, ifRobbedPrev);
+
+            ifRobbedPrev = currRobbed;
+            ifNotRobbedPrev = currNotRobbed;
+        }
+
+        return Math.max(ifRobbedPrev, ifNotRobbedPrev);
+    }
 }
